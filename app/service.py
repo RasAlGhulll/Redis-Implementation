74b98a0376c1bda71c6ea3321cf05ee1e2fe9ed9
Shreyans <shreyanssureja@gmail.com>
2020-04-20 23:00:02 +0530

    Update service.py

--- app/service.py
+++ app/service.py
@@ -1,8 +1,10 @@
 import json
+from heapq import heappush, heappop
 from sortedcontainers import SortedDict
 from datetime import datetime, timedelta
-cache = {"dummy" : [datetime.now(), "23"]}
-sortedset = {}
+cache = {}
+set = {}
+lookup = {}
 
 #method for GET command....fetching from cache dictionary
 def get_value(key):
@@ -19,35 +21,51 @@ def set_value(key, value):
 
 #inseting new value in sorted set
 def add_set(key,score,value):
-    if key in sortedset:
-        sortedset[key][score] = value
-        print(sortedset[key][score])
+
+    if value in lookup and key in lookup[value]:
+        list = set[key]
+        previous_score = lookup[value][key]
+        list.pop(previous_score,-1)
+        set[key] = list
+
+    if key in set:
+        dict = set[key]
+        if score in dict.keys():
+            heappush(dict[score],value)
+        else:
+            temp = []
+            heappush(temp,value)
+            dict[score] = temp
+        set[key] = dict
     else:
         temp = SortedDict()
-        temp[score] = value
-        sortedset[key] = temp
+        temp[score] = []
+        heappush(temp[score],value)
+        set[key] = temp
+
+    lookup[value] = {key : score}
     return "OK"
 
 def get_set():
     items = {}
-    for key in list(sortedset):
-        items[key] = sortedset[key]
+    for key in list(set):
+        items[key] = set[key]
     return items
 
 #setting expiry for perticular key in cache
 def set_expiry(key,time):
     if key in cache:
-        print(cache[key][0])
         cache[key][0] = datetime.now() + timedelta(seconds = int(time))
-        print(cache[key][0])
         return "1"
     else:
         return "0"
 
 #finding element in given range for perticular key
 def range_elements(key,left,right):
-    dict = sortedset[key]
-    items = dict.items()
+    items = []
+    for i in set[key].values():
+        items = items + list(i)
+
     left = int(left)
     right = int(right)
 
@@ -59,12 +77,14 @@ def range_elements(key,left,right):
     lower = min(left,right)
     upper = max(left,right)
 
-    print(items[lower:upper+1])
-    return "OK"
+    ans = {"values:" : items[lower:upper+1]}
+    return ans
 
 def rank(key,value):
     pivot = 0
-    list = sortedset[key].values()
+    list = []
+    for i in set[key].values():
+        list = list + i
 
     for i in list:
         if(i == value):
@@ -84,7 +104,9 @@ def save():
     with open('cache.txt', 'w') as outfile:
         json.dump(cache, outfile)
     with open('set.txt', 'w') as outfile:
-        json.dump(sortedset, outfile)
+        json.dump(set, outfile)
+    with open('lookup.txt', 'w') as outfile:
+        json.dump(lookup, outfile)
     return "OK"
 
 #loading cache data from our storage when server restarts
@@ -92,12 +114,13 @@ def onstart():
     with open('cache.txt') as json_file:
         cache = json.load(json_file)
     with open('set.txt') as json_file:
-        sortedset = json.load(json_file)
+        set = json.load(json_file)
+    with open('lookup.txt') as json_file:
+        lookup = json.load(json_file)
     return "OK"
 
 #task method run in 60 seconnds interval to remove expired cache and take backup of our cache
 def task():
     remove_expired()
     save()
-    print("I am running tsk1 at ", datetime.now())
     return "OK"
